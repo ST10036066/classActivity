46984311ed3cd693e07142fb55bdf8726151535e
Azania Aria Ncube <ST10036066@vcconnect.edu.za>
2024-07-16 22:57:08 +0200

    Add project files.

---
+++ classActivity/ClassResults.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classActivity
+{
+    
+    
+    internal class ClassResults
+    {
+        private string engName;
+        private string LtnName;
+        private double date;
+
+        public ClassResults (string engName, string ltnName, double date)
+        {
+            this.engName = engName;
+            LtnName = ltnName;
+            this.date = date;
+        }
+
+        public string EngName { get => engName; set => engName = value; }
+        public string LtnName1 { get => LtnName; set => LtnName = value; }
+        public double Date { get => date; set => date = value; }
+    
+        
+        public void brids ()
+        {
+            Console.WriteLine( "Enter english name of bird");
+            engName = Console.ReadLine();
+
+            Console.WriteLine("Enter Latin name of bird");
+            LtnName= Console.ReadLine();
+
+            Console.WriteLine("Enter the data of its last sighting");
+            date = Convert.ToDouble(Console.ReadLine());
+
+            ClassResults bd = new ClassResults(engName,LtnName, date);  
+            Console.WriteLine( " A summary of the bird" + "\n The name of the bird is" + engName +
+                " Its Latin namee is " + "The last date it was sighted was on the " + date);
+        }
+
+       
+    }
+
+}
---
+++ classActivity/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classActivity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // BirdData data = new BirdData();
+            //data.brids();
+            
+            Console.WriteLine("Please enter your final mark");
+            Double mark;//= Convert.ToDouble(Console.ReadLine());
+            while (Double.TryParse(Console.ReadLine(), out mark) == false)
+            {
+
+                Console.WriteLine("please enter your mark >>>> ");
+
+            }
+            if (mark <= 45)
+            {
+                Console.WriteLine("failed Sorry please try again");
+            }
+            else if (mark == 49)
+            {
+                Console.WriteLine("result is passed (condoned);");
+
+            }
+            else if (mark == 74)
+            {
+                Console.WriteLine("the result is passed");
+            }
+            else
+            {
+                Console.WriteLine("Distinction");
+            }
+
+            
+
+            Console.ReadKey();
+        }
+
+    }
+         
+}
